ce575533acde86e59f1dacb13a7b8c8eef264288
Viliam <vatvil@gmail.com>
2024-08-05 20:20:12 +0200

    validation user; install express-validator

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -2,7 +2,6 @@ import { auth } from "express-oauth2-jwt-bearer";
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/user";
-import { log } from "console";
 
 declare global {
   namespace Express {
